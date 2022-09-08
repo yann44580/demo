516e78a1c922bd6f5c6109d0ff9721b621a5b66a
yann FIOLLEAU <yann.fiolleau@gmail.com>
2022-09-08 10:00:39 +0200

    fix: delete @Autowired

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,29 +17,30 @@ import com.example.demo.repository.PersonRepository;
 
 @RestController
 @RequestMapping("/person")
+@Service
 public class PersonController {
 
-	@Autowired
-	PersonRepository personRepository;
+	
+	private PersonRepository personRepository;
 
 
 	@GetMapping("/")
-	public List<Person> findAll() {
+	public List<Person> findAll(PersonRepository personRepository) {
 		return this.personRepository.findAll();
 	}
 
 	@GetMapping("/{id}")
-	public Person findById(@PathVariable long id) throws Exception {
+	public Person findById(PersonRepository personRepository, @PathVariable long id) throws Exception {
 		return this.personRepository.findById(id).orElseThrow(() -> new Exception("La personne n'existe pas"));
 	}
 
 	@PostMapping("/")
-	public Person savePerson(@RequestBody Person person) {
+	public Person savePerson(PersonRepository personRepository, @RequestBody Person person) {
 		return this.personRepository.save(person);
 	}
 
 	@PutMapping("/")
-	Person updatePerson(@RequestBody Person person, @PathVariable long id) {
+	Person updatePerson(PersonRepository personRepository, @RequestBody Person person, @PathVariable long id) {
 		return this.personRepository.findById(id).map(x -> {
 			x.setName(person.getName());
 			x.setFullname(person.getFullname());
@@ -51,7 +52,7 @@ public class PersonController {
 	}
 
 	@DeleteMapping("/{id}")
-	 void deleteById(@PathVariable Long id) { 
+	 void deleteById(PersonRepository personRepository, @PathVariable Long id) { 
 		this.personRepository.deleteById(id); }
 	 
 }
